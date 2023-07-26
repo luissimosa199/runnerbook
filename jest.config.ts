e9d48ba119e373868058fb1d06e01a71da467d03
Luis Simosa <simosa37@gmail.com>
2023-07-26 13:42:31 -0300

    test: new test config added

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,6 @@ module.exports = {
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
   setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
   moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-  },
+    '^@/(.*)$': '<rootDir>/src/$1',
+  }
 };
