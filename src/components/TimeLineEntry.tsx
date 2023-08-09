37cbe5c17ba3697e0a4c178ed339a2c8efe2fb93
Luis Simosa <simosa37@gmail.com>
2023-08-09 08:58:19 -0300

    fix: caption size

--- src/components/TimeLineEntry.tsx
+++ src/components/TimeLineEntry.tsx
@@ -16,10 +16,7 @@ const TimeLineEntry: FunctionComponent<TimeLineEntryProps> = ({ data, idx, lengt
                     priority={idx === 0}
                 />
             </div>
-            <p className="text-base text-gray-500 mt-2">{data.caption}</p>
-            {idx + 1 !== length && <div className="absolute">
-                <span className="relative text-lg font-extrabold top-[1px] -z-10 text-green-500 h-12">↓</span>
-            </div>}
+            <p className="text-lg text-gray-500 mt-2 ml-2">{data.caption}</p>
         </div>
     )
 }
